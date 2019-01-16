5c206a645ae2fdb3f652806371b583e421f8f5a0
Lukortech <lukortech@gmail.com>
2019-01-16 19:16:54 +0100

    getting charts

--- assets/scripts.js
+++ assets/scripts.js
@@ -1,26 +1,3 @@
-/* The data per machine is of the following type:
-**  Machine name
-**  Variable name
-**  Datetime from
-**  Datetime to
-**  Value in that timeframe
-
-
-Each machine produces this set of data for (variable names)
-PRODUCTION: Number of pieces produced (gross output including bad ones)
-SCRAP: Number of pieces which are bad (these will be reground and fed into the machine’s PET intake again)
-CORE TEMPERATURE: The temperature in degrees Celsius of the machine.
-
-On top of that the machine will tell the DB application whether or not it is running. Every change in this state will be stored in the DB application using the following scheme:
-
-Machine name
-Datetime
-IsRunning
-
-When a machine is running it is called ‘uptime’. When a machine is not running it is called ‘downtime’.
-Assume machines in the factory are planned to run 24/7.
-*/
-
 // As a front end: [ https://www.marviq.com/assessment/index.php?page=a&from=2018-01-07%2000:00:00 ]
 
 
@@ -35,6 +12,44 @@ Assume machines in the factory are planned to run 24/7.
       dateForm.attachEvent('onsubmit', handleDaterange);            //Old IE
     }
 */
+
+/* CHART related stuff */
+
+(function chart1(...args){
+  console.log("I am off to work!");
+  var data = {
+    labels: ['Bananas', 'Apples', 'Grapes'],
+    series: [3, 2, 5],
+    somestuff : [args]
+  };
+  
+  var options = {
+    labelInterpolationFnc: function(value) {
+      return value[0]
+    }
+  };
+  
+  var responsiveOptions = [
+    ['screen and (min-width: 640px)', {
+      chartPadding: 30,
+      labelOffset: 100,
+      labelDirection: 'explode',
+      labelInterpolationFnc: function(value) {
+        return value;
+      }
+    }],
+    ['screen and (min-width: 1024px)', {
+      labelOffset: 80,
+      chartPadding: 20
+    }]
+  ];
+  
+  new Chartist.Pie('.ct-chart', data, options, responsiveOptions);
+})();
+
+
+
+
 var machinesDetailedInformation;
 (function InitializeDatabase(){
   
@@ -48,30 +63,32 @@ var machinesDetailedInformation;
     console.log(json);
     machinesDetailedInformation = json;
     (function popup(){
-      setTimeout(function(){ 
-        document.body.innerHTML = document.body.innerHTML + 
-    `<span style="position:fixed; top:0; background: #CCC; width:100%;">We have initialized the databes! you're good to go!</span>`;
-       }, 5000);
+      var flasher = `<span class="flasher">We have initialized the database! you're good to go! <i style="cursor:pointer;" class="fas fa-times" onclick="deleteFlasher();"></i></span>`;
+      document.body.innerHTML = document.body.innerHTML + flasher;
     })();
-    //this.props.dispatch(doSomethingWithResult(json)) 
   })
   .catch( err => {
     err.text().then( errorMessage => {
       console.log("We couldn't initialize the database." + errorMessage);
-      //this.props.dispatch(displayTheError(errorMessage))
     })
   })
 })();
-function filterMachines(number){
-  machinesDetailedInformation.forEach(machine => {
-    
-  });
+
+function deleteFlasher(){ 
+  document.body.removeChild(document.querySelector(".flasher"));
+};
+
+function getCertainMachine(number){
+  console.log(machinesDetailedInformation[number]);
+
+}
+
+function chosenMachine(startH, endH, name, option) {
+  this.startH = startH;
+  this.endH = endH;
+  this.name = name;
+  this.option = option;
 }
 
 
-/*
-
-document.body.innerHTML = document.body.innerHTML + 
-    `<span style="position:fixed; top:0; background: #CCC; width:100%;">We have initialized the databes! you're good to go!</span>`;
-
-*/
+// var machine1 = new chosenMachine(machinesDetailedInformation[0].H0, machinesDetailedInformation[0].H23, machinesDetailedInformation[0].MACHINE, machinesDetailedInformation[0].PRODUCTION);
