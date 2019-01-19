384876cbe8fd28f7c92a8bf85f2fcac706627062
Lukortech <lukortech@gmail.com>
2019-01-19 18:37:20 +0100

    Seeing colours!

--- README.md
+++ README.md
@@ -2,4 +2,4 @@
 
 The task I've been asigned to apply for junior position @marviq.
 
-[See it live:](https://awesome-volhard-fa9912.netlify.com/ "Marviq recruitment task")
+See it live: [here](https://awesome-volhard-fa9912.netlify.com/ "Marviq recruitment task")
--- assets/colouring.js
+++ assets/colouring.js
@@ -9,3 +9,40 @@ Use warning/orange if the temperature has been over 85, but under or equal to 10
 Use fatal/red when [higher than 100 degrees] or [higher than 85 degrees for longer than 15 minutes].
 
 */
+var MACHINESSTATES;
+(function initializeStates(){
+  fetch("https://www.marviq.com/assessment/index.php?page=b&from=2018-01-07%2000:00:00")
+  .then( response => {
+    if (!response.ok) { throw response }
+    return response.json()
+  })
+  .then( json => {
+    console.log("States: ", json);
+    MACHINESSTATES = json;
+    view_states(MACHINESSTATES);
+  })
+  .catch( err => {
+    err.text().then( errorMessage => {
+      console.error("There was an error: assessing machines states. " + errorMessage);
+    })
+  })
+})();
+
+
+
+
+function view_states(resource){
+  var smth = document.querySelector("#states"); // Place where the output will be shown.
+  
+  var smth2 = Object.entries(resource);
+  let template = function(name,value,color='green'){
+    return `<div>
+              <label style="color:${color}">State of ${name} machine: </label>
+              <input type="text" value="${value}" readonly="readonly">
+            </div>`;
+  }
+
+  smth2.forEach(machine => {
+    smth.innerHTML = smth.innerHTML + template(machine[0],machine[1].split("/")[0],machine[1].split("/")[1]);
+  });
+}
